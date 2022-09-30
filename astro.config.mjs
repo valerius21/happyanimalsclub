610c9d3ac339f8c67f2429811f5649d1f384752b
Valerius Mattfeld <mail@valerius.me>
2022-09-30 14:38:34 +0200

    fix navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://happyanimalsclub.org',
-  integrations: [mdx(), sitemap(), tailwind(),]
+  integrations: [mdx(), sitemap(), tailwind(), preact()]
 });
