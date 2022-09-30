dd29a0427818149d2f891347dddf261c864618ad
Valerius Mattfeld <mail@valerius.me>
2022-09-30 13:51:21 +0200

    update landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import mdx from '@astrojs/mdx';
-import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://happyanimalsclub.org',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  integrations: [mdx(), sitemap(), tailwind(),]
 });
